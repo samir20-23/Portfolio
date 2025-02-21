a38abdf1d445880699352c0f38cb86893d88ee10
samir20-23 <germanysamir1@gmail.com>
2025-02-21 15:51:23 +0000

    version10

---
+++ main_Apps/Portfolio_vueJs/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>About Me</h1>
+    <p>I'm a passionate developer with expertise in web development using Vue.js, HTML, CSS, JavaScript, and more.</p>
+    <p>$$$$$$$$$</p> <!-- Add your personal details or any content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.about {
+  padding: 20px;
+}
+</style>

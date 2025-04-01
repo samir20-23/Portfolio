09632390ac446f6e42c2d2ea8f956c38388a5aee
samir20-23 <aouladamarsamir@gmail.com>
2025-04-01 19:26:58 +0100

    new

--- main_Apps/Portfolio_nextJs/components/projects.tsx
+++ main_Apps/Portfolio_nextJs/components/projects.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+// import React from "react";
+// import SectionHeading from "./section-heading";
+// import { projectsData } from "@/lib/data";
+// import Project from "./project";
+// import { useSectionInView } from "@/lib/hooks";
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";

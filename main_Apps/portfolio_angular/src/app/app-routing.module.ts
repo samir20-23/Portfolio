2555610b6ae372a27fdddda0546e603232eba3c2
samir20-23 <aouladamarsamir@gmail.com>
2025-07-30 00:07:18 +0100

    create(Pages):create pages latest for test app home about contact

--- main_Apps/portfolio_angular/src/app/app-routing.module.ts
+++ main_Apps/portfolio_angular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

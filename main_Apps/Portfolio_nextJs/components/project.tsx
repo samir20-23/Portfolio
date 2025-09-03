e7d48605f18a29b44724f33c572be85c92b9b651
samir20-23 <aouladamarsamir@gmail.com>
2025-09-03 23:56:54 +0100

    update

--- main_Apps/Portfolio_nextJs/components/project.tsx
+++ main_Apps/Portfolio_nextJs/components/project.tsx
@@ -27,6 +27,7 @@ export default function Project({
     target: ref,
     offset: ["0 1", "1.33 1"],
   });
+
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
@@ -50,7 +51,7 @@ export default function Project({
       className="group mb-3 sm:mb-8 last:mb-0"
     >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <Link href={pageUrl} target="_blank">
+        <Link href={pageUrl || "#"} target="_blank">
           <div
             className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem] overflow-y-auto max-h-[18rem] scrollbar-thin scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/30 scroll-hidden"
             style={{ scrollBehavior: "smooth" }}
@@ -71,25 +72,25 @@ export default function Project({
             </ul>
           </div>
 
-          <Image
-            src={currentImage}
-            alt={`Screenshot of ${title}`}
-            width={600}
-            height={400}
-            quality={95}
-            className={`
-              absolute hidden sm:block top-16 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
-              transition
-              group-hover:scale-[1.04]
-              group-hover:-translate-x-3
-              group-hover:translate-y-3
-              group-hover:-rotate-2
-              group-even:group-hover:translate-x-3
-              group-even:group-hover:translate-y-3
-              group-even:group-even:rotate-2
-              group-even:right-[initial] group-even:-left-40
-            `}
-          />
+          {currentImage && (
+            <Image
+              src={currentImage}
+              alt={`Screenshot of ${title}`}
+              width={600}
+              height={400}
+              quality={95}
+              className={`absolute hidden sm:block top-16 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+                transition
+                group-hover:scale-[1.04]
+                group-hover:-translate-x-3
+                group-hover:translate-y-3
+                group-hover:-rotate-2
+                group-even:group-hover:translate-x-3
+                group-even:group-hover:translate-y-3
+                group-even:group-even:rotate-2
+                group-even:right-[initial] group-even:-left-40`}
+            />
+          )}
         </Link>
       </section>
     </motion.div>
